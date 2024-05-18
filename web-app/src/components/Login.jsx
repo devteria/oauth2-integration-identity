bc5a5d116881334331dc489a2082fb6ef1b3b654
devteria <devteriadotcom@gmail.com>
2024-05-18 16:17:15 +0700

    refactor code

--- web-app/src/components/Login.jsx
+++ web-app/src/components/Login.jsx
@@ -4,13 +4,19 @@ import {
   Card,
   CardActions,
   CardContent,
+  Divider,
   TextField,
   Typography,
 } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
 import { OAuthConfig } from "../configurations/configuration";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getToken } from "../services/localStorageService";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   const handleClick = () => {
     const callbackUrl = OAuthConfig.redirectUri;
     const authUrl = OAuthConfig.authUri;
@@ -24,6 +30,14 @@ export default function Login() {
 
     window.location.href = targetUrl;
   };
+
+  useEffect(() => {
+    const accessToken = getToken();
+
+    if (accessToken) {
+      navigate("/");
+    }
+  }, [navigate]);
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -47,7 +61,7 @@ export default function Login() {
       >
         <Card
           sx={{
-            minWidth: 275,
+            minWidth: 250,
             maxWidth: 400,
             boxShadow: 4,
             borderRadius: 4,
@@ -79,7 +93,7 @@ export default function Login() {
             </Box>
           </CardContent>
           <CardActions>
-            <Box display="flex" flexDirection="column" width="100%" gap="10px">
+            <Box display="flex" flexDirection="column" width="100%" gap="25px">
               <Button
                 type="submit"
                 variant="contained"
@@ -96,37 +110,19 @@ export default function Login() {
                 size="large"
                 onClick={handleClick}
                 fullWidth
-                sx={{ mt: 2, gap: "10px" }}
+                sx={{ gap: "10px" }}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 326667 333333"
-                  shapeRendering="geometricPrecision"
-                  textRendering="geometricPrecision"
-                  imageRendering="optimizeQuality"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  width={20}
-                  height={20}
-                >
-                  <path
-                    d="M326667 170370c0-13704-1112-23704-3518-34074H166667v61851h91851c-1851 15371-11851 38519-34074 54074l-311 2071 49476 38329 3428 342c31481-29074 49630-71852 49630-122593m0 0z"
-                    fill="#4285f4"
-                  />
-                  <path
-                    d="M166667 333333c44999 0 82776-14815 110370-40370l-52593-40742c-14074 9815-32963 16667-57777 16667-44074 0-81481-29073-94816-69258l-1954 166-51447 39815-673 1870c27407 54444 83704 91852 148890 91852z"
-                    fill="#34a853"
-                  />
-                  <path
-                    d="M71851 199630c-3518-10370-5555-21482-5555-32963 0-11482 2036-22593 5370-32963l-93-2209-52091-40455-1704 811C6482 114444 1 139814 1 166666s6482 52221 17777 74814l54074-41851m0 0z"
-                    fill="#fbbc04"
-                  />
-                  <path
-                    d="M166667 64444c31296 0 52406 13519 64444 24816l47037-45926C249260 16482 211666 1 166667 1 101481 1 45185 37408 17777 91852l53889 41853c13520-40185 50927-69260 95001-69260m0 0z"
-                    fill="#ea4335"
-                  />
-                </svg>
+                <GoogleIcon />
                 Continue with Google
+              </Button>
+              <Divider></Divider>
+              <Button
+                type="submit"
+                variant="contained"
+                color="success"
+                size="large"
+              >
+                Create an account
               </Button>
             </Box>
           </CardActions>
